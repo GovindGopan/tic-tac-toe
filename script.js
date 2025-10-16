e56121419b3791cdeb227ec9b4875054573d7b76
Govind Gopan <govindgopan2004@gmail.com>
2025-10-17 02:45:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.querySelector('.game-board');
+const resetButton = document.getElementById('resetButton');
+const playerXScoreElement = document.getElementById('playerXScore');
+const playerOScoreElement = document.getElementById('playerOScore');
+
+let isPlayerOTurn = false;
+let playerXScore = 0;
+let playerOScore = 0;
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+startGame();
+
+resetButton.addEventListener('click', startGame);
+
+function startGame() {
+    isPlayerOTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove('x');
+        cell.classList.remove('o');
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = isPlayerOTurn ? 'o' : 'x';
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        alert("Draw!");
+    } else {
+        alert(`${isPlayerOTurn ? "O's" : "X's"} Wins!`);
+        if (isPlayerOTurn) {
+            playerOScore++;
+            playerOScoreElement.innerText = playerOScore;
+        } else {
+            playerXScore++;
+            playerXScoreElement.innerText = playerXScore;
+        }
+    }
+    startGame();
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains('x') || cell.classList.contains('o');
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    isPlayerOTurn = !isPlayerOTurn;
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
+}
