099a55b469b9684f4077c1e5cd2d8932f36bcdeb
Govind Gopan <govindgopan2004@gmail.com>
2025-10-17 02:57:38 +0530

    feat: update dark mode toggle functionality

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ resetButton.addEventListener('click', startGame);
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     if (document.body.classList.contains('dark-mode')) {
-        themeIcon.classList.remove('fa-sun');
-        themeIcon.classList.add('fa-moon');
-    } else {
         themeIcon.classList.remove('fa-moon');
         themeIcon.classList.add('fa-sun');
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
     }
 });
 
